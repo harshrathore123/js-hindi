3f228e85a42872076fdeb20da199a85389231339
Harsh Rathore <Rathorehrharsh5@gmail.com>
2025-01-17 11:18:57 +0000

    Variable

---
+++ 00_Basic/Variable.js
@@ -0,0 +1,15 @@
+const StudentId = 12313;
+let StudentName = "Harsh"
+var StudentEmail = "Rathorehrharsh5@gmail.com"
+
+console.table({StudentId,StudentName,StudentEmail})
+
+
+const ob1 = {
+    id : "1",
+    name : "Harsh",
+    email : "rathorehrharsh@gmail.com",
+    password : "harsh12"
+};
+
+console.log(ob1.id,ob1.name,ob1.email,ob1.password);
