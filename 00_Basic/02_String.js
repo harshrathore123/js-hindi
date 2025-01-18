9133cead3378ace7b0980b0342c7e9f00b78f374
Harsh Rathore <Rathorehrharsh5@gmail.com>
2025-01-18 15:16:39 +0000

    JavaScript Data

---
+++ 00_Basic/02_String.js
@@ -0,0 +1,11 @@
+let a = "abc"
+console.log(a)
+
+let b = a.length
+console.log(b)
+
+let c = a.concat(b)
+console.log(c)
+
+let d = a.charAt(0)
+console.log(d)
---
+++ 00_Basic/03_DateInJS.js
@@ -0,0 +1,14 @@
+//Dates:
+
+const mydate = new Date();
+console.log(mydate)
+
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleDateString());
+console.log(typeof(mydate))
+
+
+let mycreatedDate = new Date(2024,0,23)
+console.log(mycreatedDate.toDateString())
